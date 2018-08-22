a3c7fd34b85b3bababf52204a73baab63c140cb2
Alex Gutierrez <0alexgutierrez@gmail.com>
2018-08-22 14:24:50 -0700

    started bonus problems

--- Bonus.js
+++ Bonus.js
@@ -5,27 +5,28 @@ const HEROES = [
   { id: 4, name: 'Superman', squad: 'Justice League' },
   { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
   { id: 6, name: 'Aquaman', squad: 'Justice League' },
-  { id: 7, name: 'Hulk', squad: 'Avengers' },
+  { id: 7, name: 'Hulk', squad: 'Avengers' }
 ];
 
 function findOne(arr, query) {
-
+  for (const item of arr) {
+    for (const key in query) {
+      console.log(item);
+    }
+  }
 }
 
-
-
-
-findOne(HEROES, { id: 1 });
-=> { id: 1, name: 'Captain America', squad: 'Avengers' }
+console.log(findOne(HEROES, { id: 1 }));
+// => { id: 1, name: 'Captain America', squad: 'Avengers' }
 
 findOne(HEROES, { id: 10 });
-=> null
+// => null
 
 findOne(HEROES, { id: 2, name: 'Aquaman' });
-=> null
+// => null
 
 findOne(HEROES, { id: 5, squad: 'Justice League' });
-=> { id: 5, name: 'Wonder Woman', squad: 'Justice League' }
+// => { id: 5, name: 'Wonder Woman', squad: 'Justice League' }
 
 findOne(HEROES, { squad: 'Justice League' });
-=> { id: 4, name: 'Superman', squad: 'Justice League' }
+// => { id: 4, name: 'Superman', squad: 'Justice League' }
